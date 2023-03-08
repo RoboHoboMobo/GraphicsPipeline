10bf8a1ad69770776e7c3227537d25ec883c6287
RoboHoboMobo <0xrobohobomobo@gmail.com>
2023-03-08 16:50:19 +0200

    Add shaders

--- GraphicsPipeline/main.cpp
+++ GraphicsPipeline/main.cpp
@@ -7,9 +7,73 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
+const std::string vertexShader =
+  "#version 330 core\n"
+  "\n"
+  "layout(location = 0) in vec4 position;\n" // because gl_Position is vec4
+  "\n"
+  "void main()\n"
+  "{\n"
+  "   gl_Position = position;\n"
+  "}\n";
 
+const std::string fragmentShader =
+  "#version 330 core\n"
+  "\n"
+  "layout(location = 0) out vec4 color;\n"
+  "\n"
+  "void main()\n"
+  "{\n"
+  "   color = vec4(0.5, 0.5, 0.5, 1.0);\n"
+  "}\n";
+
+
+static unsigned int compileShader(unsigned int shaderType, const std::string& source)
+{
+  unsigned int id = glCreateShader(shaderType);
+  const char* src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  int result{};
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+  if (!result) {
+    int len{};
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+    char* message = static_cast<char*>(alloca(len * sizeof(char)));
+    glGetShaderInfoLog(id, len, &len, message);
+
+    std::string typeString = shaderType == GL_VERTEX_SHADER ? "vertex" : "fragment";
+
+    std::cerr << "Failed to compile " << typeString << "shader"<< std::endl;
+    std::cerr << message << std::endl;
+
+    glDeleteShader(id);
+
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader,
+                                 const std::string& fragmentShader)
+{
+  unsigned int prog = glCreateProgram();
+  unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(prog, vs);
+  glAttachShader(prog, fs);
+
+  glLinkProgram(prog);
+  glValidateProgram(prog);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return prog;
 }
 
 int main()
@@ -71,6 +135,9 @@ int main()
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  unsigned int shader = createShader(vertexShader, fragmentShader);
+  glUseProgram(shader);
 
   while (!glfwWindowShouldClose(window)) {
     cap >> img;
