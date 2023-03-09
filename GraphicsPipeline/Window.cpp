f282f451149b6a17eeab88b95ac86deeecf777c9
RoboHoboMobo <0xrobohobomobo@gmail.com>
2023-03-09 22:43:10 +0200

    Add wrapper for window

---
+++ GraphicsPipeline/Window.cpp
@@ -0,0 +1,44 @@
+#include "Window.h"
+
+#include <iostream>
+#include <system_error>
+
+GlfwWindow::GlfwWindow(int width, int height, const std::string& name)
+  : m_window{}
+{
+  if (!glfwInit())
+    throw std::system_error({}, "Failed to initialize GLFW");
+
+  m_window = glfwCreateWindow(width, height, "TestOpenGL", {}, {});
+
+  if (!m_window) {
+    glfwTerminate();
+
+    throw std::system_error({}, "Failed to open GLFW window");
+  }
+
+  glfwMakeContextCurrent(m_window);
+}
+
+GlfwWindow::~GlfwWindow()
+{
+  if (m_window) {
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+  }
+}
+
+bool GlfwWindow::isShouldBeClosed() const
+{
+  return glfwWindowShouldClose(m_window);
+}
+
+void GlfwWindow::swapBuffer()
+{
+  glfwSwapBuffers(m_window);
+}
+
+void GlfwWindow::pollEvents()
+{
+  glfwPollEvents();
+}
--- GraphicsPipeline/main.cpp
+++ GraphicsPipeline/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <memory>
 
 // OpenCV
 #include <opencv2/opencv.hpp>
 
 // OpenGL
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
 
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Renderer.h"
+#include "Window.h"
 
 int main()
 {
@@ -30,25 +31,7 @@ int main()
     return -1;
   }
 
-  GLFWwindow* window;
-
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-
-    return -1;
-  }
-
-  window = glfwCreateWindow(width, height, "TestOpenGL", {}, {});
-
-  if (!window) {
-    std::cerr << "Failed to open GLFW window" << std::endl;
-
-    glfwTerminate();
-
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
+  std::shared_ptr<Window> window = std::make_shared<GlfwWindow>(width, height, "TestOpenGL");
 
   if (glewInit() != GLEW_OK) {
     std::cerr << "Failed to initialize GLEW" << std::endl;
@@ -56,68 +39,63 @@ int main()
     return -1;
   }
 
-  { // scope in order to call ~VertexBuffer() before context destruction
-    const std::vector<float> positions{
-      -0.5f, -0.5f,
-       0.5f,  0.5f,
-       0.5f, -0.5f,
-      -0.5f,  0.5f
-    };
+  const std::vector<float> positions{
+    -0.5f, -0.5f,
+     0.5f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f,  0.5f
+  };
 
-    const std::vector<unsigned int> indicies{
-      0, 1, 2,
-      0, 1, 3
-    };
+  const std::vector<unsigned int> indicies{
+    0, 1, 2,
+    0, 1, 3
+  };
 
-    // Vertex Array
-    VertexArray va;
+  // Vertex Array
+  VertexArray va;
 
-    // Vertex buffer
-    VertexBuffer vb(positions.data(), sizeof(float) * positions.size());
+  // Vertex buffer
+  VertexBuffer vb(positions.data(), sizeof(float) * positions.size());
 
-    VertexBufferLayout layout;
-    layout.push<float>(2);
+  VertexBufferLayout layout;
+  layout.push<float>(2);
 
-    va.addBuffer(vb, layout);
+  va.addBuffer(vb, layout);
 
-    // Index (element) buffer
-    IndexBuffer ib(indicies.data(), indicies.size());
+  // Index (element) buffer
+  IndexBuffer ib(indicies.data(), indicies.size());
 
-    // Shaders
-    Shader shader("res/shaders/shader.vertex", "res/shaders/shader.fragment");
-    shader.bind();
+  // Shaders
+  Shader shader("res/shaders/shader.vertex", "res/shaders/shader.fragment");
+  shader.bind();
 
-    Renderer renderer;
+  Renderer renderer;
 
-    // Unbind all
-    va.unbind();
-    shader.unbind();
-    vb.unbind();
-    ib.unbind();
+  // Unbind all
+  va.unbind();
+  shader.unbind();
+  vb.unbind();
+  ib.unbind();
 
-    while (!glfwWindowShouldClose(window)) {
-      cap >> img;
+  while (!window->isShouldBeClosed()) {
+    cap >> img;
 
-      if (img.empty())
-        continue;
+    if (img.empty())
+      continue;
 
-      cv::imshow(windowName, img);
-      cv::waitKey(25);
+    cv::imshow(windowName, img);
+    cv::waitKey(25);
 
-      renderer.setClearColor({0.3, 0.8, 0.3, 1.0});
-      renderer.clear();
+    renderer.setClearColor({0.3, 0.8, 0.3, 1.0});
+    renderer.clear();
 
-      renderer.draw(va, ib, shader);
+    renderer.draw(va, ib, shader);
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-    }
+    window->swapBuffer();
+    window->pollEvents();
   }
 
   cap.release();
-
-  glfwDestroyWindow(window);
-  glfwTerminate();
 
   return 0;
 }
