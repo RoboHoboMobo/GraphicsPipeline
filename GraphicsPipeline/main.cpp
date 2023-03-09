9c76c1535a115b8b8d9235c67580c87ecf493fd1
RoboHoboMobo <0xrobohobomobo@gmail.com>
2023-03-09 13:13:40 +0200

    Add square

--- GraphicsPipeline/main.cpp
+++ GraphicsPipeline/main.cpp
@@ -121,22 +121,31 @@ int main()
     return -1;
   }
 
-  // Triangle section
+  // Square section
   float positions[] = {
     -0.5f, -0.5f,
-     0.0f,  0.5f,
-     0.5f, -0.5f
+     0.5f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f,  0.5f
+  };
+
+  unsigned char indicies[] = {
+    0, 1, 2,
+    0, 1, 3
   };
 
   unsigned int buffer;
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  unsigned int ibo;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
   const std::string vertexShader = parseShader("res/shaders/shader.vertex");
   const std::string fragmentShader = parseShader("res/shaders/shader.fragment");
@@ -155,7 +164,7 @@ int main()
     glClearColor(0.2, 0.7, 0.2, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, nullptr);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
